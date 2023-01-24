a44078ab80c2af36ae555b90157b52e163c37233
tamtam-time <even.a.worm.will.tur@gmail.com>
2023-01-24 20:13:58 +0900

    商品出品機能　実装
    以下、確認用画像になります。
    https://gyazo.com/da7237f7f8e961c98ce0c5ff36b9cf10
    
    https://gyazo.com/469d02e036b1ee66026975f757831da5
    
    https://gyazo.com/b4cc781b4df9c5e93d5f9074b4a3687e
    
    https://gyazo.com/360970b74f2428018ee3298a45f0b6c9
    
    https://gyazo.com/d99e74bd0235412dcfd5146a0f47bc0b
    
    https://gyazo.com/3069bb6ada58f8b0aaa9ce6e128f5701
    
    宜しくお願いいたします。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,20 +1,33 @@
 class ItemsController < ApplicationController
-  # before_action :move_to_index, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
+    @item = Item.includes(:user).order('created_at DESC')
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def show
 
   end
 
-  #def show
 
-  #end
+  private
 
-  #private
+  def item_params
+    params.require(:item).permit(:image, :item_name, :explain, :category_id, :status_id, :postage_id, :area_id, :send_day_id, :price).merge(user_id: current_user.id)
+  end
 
-  #def move_to_index
-  #  unless user_signed_in?
-  #    redirect_to action: :index
-  #  end
-  #end
 
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,19 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+window.addEventListener('load', () => {
+  const priceInput = document.getElementById("item-price");
+ //  console.log(priceInput)
+   priceInput.addEventListener("input", () => {
+     const addTaxDom = document.getElementById("add-tax-price");
+     // console.log(priceInput.innerHTML);
+     addTaxDom.innerHTML = Math.round(priceInput.value * 0.1 );
+     const addPofitDom = document.getElementById("profit");
+     // console.log(priceInput.innerHTML);
+     addPofitDom.innerHTML = Math.round(priceInput.value - Math.round(priceInput.value * 0.1 ))
+ })
+});
+
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
@@ -14,3 +27,5 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+
