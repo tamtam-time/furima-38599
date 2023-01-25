9113d5ec95bc4e29c12a78185c2c71f8d07781cb
tamtam-time <even.a.worm.will.tur@gmail.com>
2023-01-25 17:12:22 +0900

    商品一覧表示機能
    商品のデータがない場合は、ダミー商品が表示されている動画
    https://gyazo.com/f7f50d1867ff3872b87e0c11ab1cda87
    
    商品のデータがある場合は、商品が一覧で表示されている動画
    https://gyazo.com/c177560ceb45543e5d6d86387ce81d70

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,9 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index] #, :show]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show]
 
   def index
-   # @items = Item.includes(:user).order('created_at DESC')
+   @items = Item.includes(:user).order('created_at DESC')
   end
 
   def new
@@ -18,14 +19,18 @@ class ItemsController < ApplicationController
     end
   end
 
-  # def show
-  # end
+   def show
+   end
 
 
   private
 
   def item_params
     params.require(:item).permit(:image, :item_name, :explain, :category_id, :status_id, :postage_id, :area_id, :send_day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
 
