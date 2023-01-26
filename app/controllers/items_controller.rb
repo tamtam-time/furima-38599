cb8c2434c923398b7c167edb68b900008650a815
tamtam-time <even.a.worm.will.tur@gmail.com>
2023-01-26 16:57:03 +0900

    商品編集機能　実装
    ログイン状態の出品者は、商品情報編集ページに遷移できる動画
    https://gyazo.com/46c7d97dd606529e8ef7cd8d6f39b5dc
    ※Gyazoの録画時間の都合上、商品詳細ページへの推移部分をカットしております。
    
    必要な情報を適切に入力して「更新する」ボタンを押すと、商品の情報を編集できる動画
    https://gyazo.com/a59668a778936c40fb95a19a841df98c
    
    入力に問題がある状態で「変更する」ボタンが押された場合、情報は保存されず、編集ページに戻りエラーメッセージが表示される動画
    https://gyazo.com/cc3d32afa56f71545a60224819672466
    
    何も編集せずに「更新する」ボタンを押しても、画像無しの商品にならない動画
    https://gyazo.com/ef1f5fc47aa328d8338cd606e5e91b43
    
    ログイン状態の場合でも、URLを直接入力して自身が出品していない商品の商品情報編集ページへ遷移しようとすると、商品の販売状況に関わらずトップページに遷移する動画
    https://gyazo.com/047a02e92462dfdf068c89f55bb1d351
    
    ログイン状態の場合でも、URLを直接入力して自身が出品した売却済み商品の商品情報編集ページへ遷移しようとすると、トップページに遷移する動画（現段階で商品購入機能の実装が済んでいる場合）
    現段階で商品購入機能、未実装です。
    
    ログアウト状態の場合は、URLを直接入力して商品情報編集ページへ遷移しようとすると、商品の販売状況に関わらずログインページに遷移する動画
    https://gyazo.com/ec56deaec0bfecba0d3e54cd855c2e7c
    
    商品名やカテゴリーの情報など、すでに登録されている商品情報は商品情報編集画面を開いた時点で表示される動画（商品画像・販売手数料・販売利益に関しては、表示されない状態で良い）
    https://gyazo.com/c7ed9149b7489d1239126ab1e0f3088b

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
    @items = Item.includes(:user).order('created_at DESC')
@@ -19,10 +19,26 @@ class ItemsController < ApplicationController
     end
   end
 
-
-
   def show
   end
+
+  def edit
+    if @item.user_id == current_user.id && @item.order.nil?
+    else
+      redirect_to root_path
+    end
+  end
+
+  def update
+    @item.update(item_params)
+    if @item.valid?
+      redirect_to item_path(item_params)
+    else
+      render 'edit'
+    end
+  end
+
+
 
 
   private
