9088575fcedbfb99711bff1ad93f53c5dfb28036
tamtam-time <even.a.worm.will.tur@gmail.com>
2023-01-26 15:48:14 +0900

    商品詳細表示機能　修正分

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,20 +1,20 @@
-class OrdersController < ApplicationController
-  before_action :authenticate_user!
-  before_action :non_purchased_item, only: [:index]
+#class OrdersController < ApplicationController
+#  before_action :authenticate_user!
+#  before_action :non_purchased_item, only: [:index]
 
-  def index
+#  def index
 
-  end
-
-
+#  end
 
 
-  private
 
 
-  def non_purchased_item
-    @item = Item.find(params[:item_id])
-    redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
-  end
+#  private
 
-end
+
+#  def non_purchased_item
+#    @item = Item.find(params[:item_id])
+#    redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
+#  end
+
+#end
