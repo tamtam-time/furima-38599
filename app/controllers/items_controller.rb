7c9515bfd15db087d80ae3ca590c82b7029fc9d7
tamtam-time <even.a.worm.will.tur@gmail.com>
2023-01-27 15:19:19 +0900

    商品削除機能　修正分

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :move_to_index, except: [:index, :show, :update]
 
   def index
    @items = Item.includes(:user).order('created_at DESC')
@@ -41,8 +42,6 @@ class ItemsController < ApplicationController
     if @item.user_id == current_user.id
       @item.destroy
       redirect_to root_path
-    else
-      redirect_to root_path
     end
   end
 
@@ -55,6 +54,12 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 
 
