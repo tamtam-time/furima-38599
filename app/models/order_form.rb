9cfad0395da63049563a50eba1c6549d27061b8a
tamtam-time <even.a.worm.will.tur@gmail.com>
2023-01-29 14:53:58 +0900

    商品購入機能　修正分
    以下、最新のテスト結果画像になります
    https://gyazo.com/8cc561d79a591884749bc13332f795d2

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -10,7 +10,7 @@
       validates :area_id, numericality: { other_than: 0, message: "can't be blank" }
       validates :municipaldistrict
       validates :district
-      validates :phone, length: {minimum: 10, maximum: 11}, format: { with: /\A[0-9]{11}\z/, message: 'is invalid' }
+      validates :phone, format: { with: /\A\d{10,11}\z/, message: 'is invalid' }
   
     end
   
